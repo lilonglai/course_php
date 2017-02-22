9df7f5d7f26f2bf4677943493867624fe7ec1b28
Kevin Li <longlai.li@oracle.com>
2017-02-22 21:27:05 +0800

    add student db operation

--- db.php
+++ db.php
@@ -97,10 +97,10 @@ class PDOSecondCourseOperation extends PDOBaseOperation {
     }
 }
 
+/*
 $test = new PDOSecondCourseOperation();
 $test->getByGrade(1);
 
-/*
 $secondCourse = array();
 $secondCourse['name']="good";
 $secondCourse['shortname']="g";
@@ -113,3 +113,81 @@ $secondCourse['description']="good is for test2";
 $test->update($secondCourse);
 
 */
+
+class PDOStudentOperation extends PDOBaseOperation {
+    const TABLENAME = "student";
+    public function getByName($name){
+        $sql = "select * from ". self::TABLENAME . " where name = '$name'";
+        $result = $this->executeSql($sql);
+        return $result;
+    }
+
+    public function getByGrade($grade){
+        $sql = "select * from ". self::TABLENAME . " where grade = $grade";
+        $result = $this->executeSql($sql);
+        return $result;
+    }
+
+    public function getAlive(){
+        $sql = "select * from ". self::TABLENAME . " where isalive = true";
+        $result = $this->executeSql($sql);
+        return $result;
+    }
+
+    public function getNotAlive(){
+        $sql = "select * from ". self::TABLENAME . " where isalive = false";
+        $result = $this->executeSql($sql);
+        return $result;
+    }
+
+    public function getByTeacherId($teacherId){
+        $sql = "select * from ". self::TABLENAME . " where teacherid = $teacherId";
+        $result = $this->executeSql($sql);
+        return $result;
+    }
+
+    public function add($student){
+        $sql = "insert into " . self::TABLENAME . "(name,shortname,grade,testscore,targetscore,examinedate,examineplace,teacherid,description) values(
+        '$student[name]', '$student[shortname]', $student[grade], '$student[testscore]', '$student[targetscore]', '$student[examinedate]', '$student[examineplace]', $student[teacherid], '$student[description]' )";
+        $this->executeUpdateSql($sql);
+    }
+
+    public function update($student){
+        $sql = "update " . self::TABLENAME . " set 
+        name='$student[name]', shortname= '$student[shortname]', grade=$student[grade], testscore='$student[testscore]', 
+        targetscore='$student[targetscore]', examinedate='$student[examinedate]', examineplace='$student[examineplace]', teacherid=$student[teacherid], description='$student[description]' 
+        where id=$student[id]";
+        $this->executeUpdateSql($sql);
+    }
+
+    public function retire($key){
+        $sql = "update " . self::TABLENAME . " set isalive=false where id=$key";
+        $this->executeUpdateSql($sql);
+    }
+}
+
+$test = new PDOStudentOperation();
+$test->getAlive();
+$test->getNotAlive();
+
+$test->getByGrade(1);
+
+$test->getByTeacherId(5);
+
+$test->getByName("陈思汝");
+
+$student = array();
+$student['name'] = 'test';
+$student['shortname'] = 'test';
+$student['grade'] = 1;
+$student['testscore'] = "50";
+$student['targetscore'] = "59";
+$student['teacherid'] = 1;
+$student['examinedate'] = "1985-06-20";
+$student['examineplace'] = "SH";
+$student['description'] = "this is the test";
+//$test->add($student);
+
+$student['description'] = "this is the test4";
+$student['id'] = 94;
+$test->update($student);
